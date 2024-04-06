05d26bfda9e5dfc179d07b23d41570acbf9245f1
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-04-06 15:37:15 -0500

    Create user model, set up app

--- server/app.py
+++ server/app.py
@@ -1,4 +1,35 @@
-from config import app
+from flask import request, session
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from config import app, db, api
+from models.models import *
+
+
+  
+
+@app.route('/api')
+def index():
+  return '<h1>Hi, I\'m John</h1>'
+
+class Signup(Resource):
+  
+  def post(self):
+    # Retrieve form inputs
+    new_user = User(
+      first_name=request.form.get('first_name'),
+      last_name=request.form.get('last_name'),
+      email=request.form.get('email')
+    )
+    new_user.password_hash = request.form.get('password')
+    try:
+      db.session.add(new_user)
+      db.session.commit()
+      session['user_id'] = new_user.id
+      return new_user.to_dict(), 201
+    except IntegrityError as e:
+      return {'error': '422 Unprocessable Entity'}, 422
+    
+api.add_resource(Signup, '/api/signup')
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
