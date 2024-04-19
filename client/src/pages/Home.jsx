de8e6a143c708e673e260e746b80eadd0e4b2219
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-04-19 00:41:54 -0500

    Fix Home so that only logged in users can view items

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,31 +2,34 @@ import { useEffect, useState } from "react";
 import StyledTitle from "../components/StyledTitle";
 import ItemCard from "../components/ItemCard";
 
-function Home( {user} ) {
+function Home({ user }) {
     const [items, setItems] = useState([])
     const welcomeTitle = user ? `Welcome, ${user.first_name}!` : "Welcome to Itemizer!";
 
     useEffect(() => {
         fetch('/api/items')
-        .then((response) => response.json())
-        .then((data) => setItems(data))
+            .then((response) => response.json())
+            .then((data) => setItems(data))
     }, [])
 
     const itemList = items.map((item) => {
         return (
-            <li key={item.id}><ItemCard item={item}/></li>
+            <li key={item.id}><ItemCard item={item} /></li>
         )
     });
 
     return (
         <div>
-            <StyledTitle text={welcomeTitle}/>
-            <div id="item-list-container">
-                <h4 id="item-list-title">Items Being Used Worldwide</h4>
-                <ul id="item-list">
-                    {itemList}
-                </ul>
-            </div>
+            <StyledTitle text={welcomeTitle} />
+            {user ? (
+                <div id="item-list-container">
+                    <h4 id="item-list-title">Items Being Used Worldwide</h4>
+                    <ul id="item-list">
+                        {itemList}
+                    </ul>
+                </div>
+                ) : <h1>Log in to manage items and organizations!</h1>
+            }
         </div>
     )
 }
